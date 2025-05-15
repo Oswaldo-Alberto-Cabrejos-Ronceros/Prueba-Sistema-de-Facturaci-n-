69852edfb51234d09c52b689984d0135c1b21dcb
Oswaldo <U22209742@utp.edu.pe>
2025-05-15 00:25:31 -0500

    feat:Crecion de componente input-primary

--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonPrimaryComponent } from '../../components/button-primary/button-primary.component';
+import { InputPrimaryComponent } from '../../components/input-primary/input-primary.component';
 @Component({
   selector: 'app-login-page',
-  imports: [ButtonPrimaryComponent],
+  imports: [ButtonPrimaryComponent, InputPrimaryComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
