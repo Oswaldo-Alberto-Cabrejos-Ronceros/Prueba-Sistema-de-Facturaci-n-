b9cbe67626775f7b493d4e97a71b20f09d791f3e
Oswaldo <U22209742@utp.edu.pe>
2025-05-15 20:01:42 -0500

    feat:Implementacion de boton para retroceder en RegisterPage

--- src/pages/register-page/register-page.component.ts
+++ src/pages/register-page/register-page.component.ts
@@ -2,18 +2,26 @@ import { Component, HostBinding, signal } from '@angular/core';
 import { RegistrationCardComponent } from '../../components/registration-card/registration-card.component';
 import { CommonModule } from '@angular/common';
 import { RegistrationCompanyDataCardComponent } from '../../components/registration-company-data-card/registration-company-data-card.component';
+import { ButtonIconComponent } from '../../components/button-icon/button-icon.component';
 @Component({
   selector: 'app-register-page',
-  imports: [RegistrationCardComponent, CommonModule, RegistrationCompanyDataCardComponent],
+  imports: [
+    RegistrationCardComponent,
+    CommonModule,
+    RegistrationCompanyDataCardComponent,
+    ButtonIconComponent,
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
-  @HostBinding('class') class =
-    'flex flex-col flex-1 items-center ';
+  @HostBinding('class') class = 'flex flex-col flex-1 items-center ';
   //variable reactiva para definir el numero de paso
   stepNumber = signal<number>(1);
   nextStep = () => {
     this.stepNumber.update((value) => value + 1);
+  };
+  previousStep = () => {
+    this.stepNumber.update((value) => (value > 0 ? value - 1 : value));
   };
 }
