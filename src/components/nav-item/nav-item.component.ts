05227ea16e4d0d410e07b9226963e3b899d6db89
Oswaldo <U22209742@utp.edu.pe>
2025-05-16 21:22:32 -0500

    feat:Adecuacion de nav-item y button-icon-label-secondary para cuando se pliegue y despliegue el menu

--- src/components/nav-item/nav-item.component.ts
+++ src/components/nav-item/nav-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, HostBinding, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-nav-item',
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule,CommonModule],
   templateUrl: './nav-item.component.html',
   styleUrl: './nav-item.component.css',
 })
@@ -12,4 +13,6 @@ export class NavItemComponent {
   @Input() to: string = '';
   @Input() title: string = '';
   @Input() iconName: string = '';
+  //para saber si esta deplegado o no
+  @Input() isDisplayed:boolean=true
 }
