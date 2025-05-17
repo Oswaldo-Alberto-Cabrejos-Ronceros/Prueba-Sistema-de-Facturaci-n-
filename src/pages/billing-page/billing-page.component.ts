f00d732c4f58add3ea2a7edbffc719b25e327b22
Oswaldo <U22209742@utp.edu.pe>
2025-05-17 17:21:48 -0500

    feat:Definicion de customRightTemplate en billing-page

--- src/pages/billing-page/billing-page.component.ts
+++ src/pages/billing-page/billing-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, TemplateRef, ViewChild } from '@angular/core';
+import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
+import { ButtonIconLabelTerciaryComponent } from '../../components/button-icon-label-terciary/button-icon-label-terciary.component';
 
 @Component({
   selector: 'app-billing-page',
-  imports: [],
+  imports: [ButtonIconLabelTerciaryComponent],
   templateUrl: './billing-page.component.html',
   styleUrl: './billing-page.component.css'
 })
 export class BillingPageComponent {
+@HostBinding('class') class = 'flex-1 flex flex-col'
+
+  //referencia al rightTemplatePersonalizado
+  @ViewChild('customRightTemplate') customRightTemplate!:TemplateRef<any>
+  //inyectamos servicio template-injector
+  constructor(private navBarUserTemplateInjector: NavBarUserTemplateInjectorService){
+  }
+  ngAfterViewInit(): void {
+    this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+  }
 
 }
