e18b7bfe4b9f6b1fc5d5b8fc667f9ad30305054e
Oswaldo <U22209742@utp.edu.pe>
2025-05-18 13:24:08 -0500

    fix:Arreglo de bug en nav-bar-user, debido a la propiedad de referencia del boton que abre opciones de usuario

--- src/components/nav-bar-user/nav-bar-user.component.ts
+++ src/components/nav-bar-user/nav-bar-user.component.ts
@@ -1,36 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component, ContentChild, ElementRef, HostBinding, TemplateRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { ButtonIconLabelTerciaryComponent } from '../button-icon-label-terciary/button-icon-label-terciary.component';
-import { PopUpMyUserAcountComponent } from "../pop-up-my-user-acount/pop-up-my-user-acount.component";
+import { PopUpMyUserAcountComponent } from '../pop-up-my-user-acount/pop-up-my-user-acount.component';
 import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
-
 
 @Component({
   selector: 'app-nav-bar-user',
-  imports: [CommonModule, ButtonIconLabelTerciaryComponent, PopUpMyUserAcountComponent],
+  imports: [
+    CommonModule,
+    ButtonIconLabelTerciaryComponent,
+    PopUpMyUserAcountComponent,
+  ],
   templateUrl: './nav-bar-user.component.html',
-  styleUrl: './nav-bar-user.component.css'
+  styleUrl: './nav-bar-user.component.css',
 })
-export class NavBarUserComponent {
-@HostBinding('class') class = 'w-full h-min';
-//referencias de los templates
-  @ContentChild('leftTemplate', { static: false }) leftTemplate!: TemplateRef<any>;
-  rightTemplate: TemplateRef<any>|null=null;
+export class NavBarUserComponent implements AfterViewInit {
+  @HostBinding('class') class = 'w-full h-min';
+  //referencias de los templates
+  @ContentChild('leftTemplate', { static: false })
+  leftTemplate!: TemplateRef<any>;
+  rightTemplate: TemplateRef<any> | null = null;
   //para pasar la referencia al componente
-@ViewChild('triggerMyUser', { read: ElementRef }) triggerMyUserRef!: ElementRef;
+  @ViewChild('triggerMyUser', { read: ElementRef })
+  triggerMyUserRef!: ElementRef;
 
   //para obtener el rightTemplate del servicio template-injector
-  constructor( private navBarUseTemplateInjector: NavBarUserTemplateInjectorService){
-    this.navBarUseTemplateInjector.rightTemplate$.subscribe(template=>{
-this.rightTemplate = template
-    })
+  constructor(
+    private navBarUseTemplateInjector: NavBarUserTemplateInjectorService,
+    //inyectamos la referencia del detecotr de cambios
+    private cdr: ChangeDetectorRef
+  ) {
+    this.navBarUseTemplateInjector.rightTemplate$.subscribe((template) => {
+      this.rightTemplate = template;
+    });
   }
 
-//para mostrar o no el pop up
-showPopup = false;
-//para variar showPopUp
-handlePopup() {
-  this.showPopup = !this.showPopup;
-}
+  //depues de iniciada la vista
+  ngAfterViewInit(): void {
+    //forzamos la deteccion de cambios
+    this.cdr.detectChanges();
+  }
 
+  //para mostrar o no el pop up
+  showPopup = false;
+  //para variar showPopUp
+  handlePopup() {
+    this.showPopup = !this.showPopup;
+  }
 }
