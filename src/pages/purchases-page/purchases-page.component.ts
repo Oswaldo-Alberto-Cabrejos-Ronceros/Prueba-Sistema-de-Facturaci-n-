24ed105eefafd0b5a5d3298ffb3af028b8f0fdad
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-05-19 13:04:10 -0500

    feat:Desarrollo de purchases-page

--- src/pages/purchases-page/purchases-page.component.ts
+++ src/pages/purchases-page/purchases-page.component.ts
@@ -1,11 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostBinding, TemplateRef, ViewChild } from '@angular/core';
+import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
+import { ButtonIconLabelQuaternaryComponent } from "../../components/button-icon-label-quaternary/button-icon-label-quaternary.component";
+import { ButtonIconLabelTerciaryComponent } from "../../components/button-icon-label-terciary/button-icon-label-terciary.component";
+import { PopUpOptionsComponent } from "../../components/pop-up-options/pop-up-options.component";
+import { DatePickerSetComponent } from "../../components/date-picker-set/date-picker-set.component";
+import { TableFilterComponent } from "../../components/table-filter/table-filter.component";
 
 @Component({
   selector: 'app-purchases-page',
-  imports: [],
+  imports: [ButtonIconLabelQuaternaryComponent, ButtonIconLabelTerciaryComponent, PopUpOptionsComponent, DatePickerSetComponent, TableFilterComponent],
   templateUrl: './purchases-page.component.html',
   styleUrl: './purchases-page.component.css'
 })
 export class PurchasesPageComponent {
+ @HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
+   //referencia al rightTemplatePersonalizado
+  @ViewChild('customRightTemplate') customRightTemplate!: TemplateRef<any>;
+  //referencia del elemento que activa opciones de exportar
+  @ViewChild('triggerExport', { read: ElementRef })
+  triggerExportRef!: ElementRef;
+  //referencia del elemento que activa carta de notificaciones
+  @ViewChild('triggerNotifications', { read: ElementRef })
+  triggerNotificationsRef!: ElementRef;
+
+    //inyectamos servicio template-injector
+    constructor(
+      private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
+    ) {}
+    ngAfterViewInit(): void {
+      this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+    }
+
+    
+  //sobre exportar
+
+  optionsExport: { title: string }[] = [
+    {
+      title: 'Exportar compras',
+    },
+    {
+      title: 'Exportar pagos detallados',
+    },
+  ];
+
+  //para mostrar opciones de exportacion
+  showPopUpExport = false;
+  //para variar showPopUpExport
+  handlePopUpExport() {
+    this.showPopUpExport = !this.showPopUpExport;
+  }
+
+  //para mostrar carta de notificaciones
+  showPopUpNotifications = false;
+  handlePopUpNotifications() {
+    this.showPopUpNotifications = !this.showPopUpNotifications;
+  }
+
+
+  //sobre tabla
+  columns: {
+    key: string;
+    label: string;
+    typeFilter: string;
+    disable?: boolean;
+    options?: string[];
+    ordenable?: boolean;
+  }[] = [
+    {
+      key: 'purchases',
+      label: 'Compra',
+      typeFilter: 'input',
+    },
+    {
+      key: 'document',
+      label: 'Documento',
+      typeFilter: 'input',
+    },
+        {
+      key: 'provider',
+      label: 'Provedor',
+      typeFilter: 'input',
+    },
+    {
+      key: 'state',
+      label: 'Total',
+      typeFilter: 'select',
+      options: ['Todos', 'Pagado', 'Deuda'],
+    },
+    {
+      key: 'date',
+      label: 'Fecha',
+      typeFilter: 'input',
+      disable: true,
+    },
+
+    {
+      key: 'users',
+      label: 'Usuario',
+      typeFilter: 'input',
+    }
+  ];
+
+  //para la activacion de la tabla
+  isTableEnabled = false;
+
+  //para manejar la activacion de la tabla
+  handleEnabledTable = () => {
+    this.isTableEnabled = !this.isTableEnabled;
+  };
 
 }
