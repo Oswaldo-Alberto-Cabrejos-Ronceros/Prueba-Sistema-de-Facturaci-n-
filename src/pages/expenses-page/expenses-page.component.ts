1cb8c14fd2f9efdf1a839b6a6e274d30e5fbca19
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-05-20 12:59:49 -0500

    feat:Desarrollo de exponses-page

--- src/pages/expenses-page/expenses-page.component.ts
+++ src/pages/expenses-page/expenses-page.component.ts
@@ -1,11 +1,107 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
+import { ButtonIconLabelQuaternaryComponent } from '../../components/button-icon-label-quaternary/button-icon-label-quaternary.component';
+import { ButtonIconLabelTerciaryComponent } from '../../components/button-icon-label-terciary/button-icon-label-terciary.component';
+import { PopUpOptionsComponent } from '../../components/pop-up-options/pop-up-options.component';
+import { CommonModule } from '@angular/common';
+import { DatePickerSetComponent } from '../../components/date-picker-set/date-picker-set.component';
+import { TableFilterComponent } from '../../components/table-filter/table-filter.component';
 
 @Component({
   selector: 'app-expenses-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonIconLabelQuaternaryComponent,
+    ButtonIconLabelTerciaryComponent,
+    PopUpOptionsComponent,
+    DatePickerSetComponent,
+    TableFilterComponent,
+  ],
   templateUrl: './expenses-page.component.html',
-  styleUrl: './expenses-page.component.css'
+  styleUrl: './expenses-page.component.css',
 })
 export class ExpensesPageComponent {
+  @HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
 
+  //referencia del elemento que activa opciones de exportar
+  @ViewChild('triggerExport', { read: ElementRef })
+  triggerExportRef!: ElementRef;
+  //referencia al rightTemplatePersonalizado
+  @ViewChild('customRightTemplate', { static: true })
+  customRightTemplate!: TemplateRef<any>;
+
+  //inyectamos servicio template-injector
+  constructor(
+    private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
+  ) {}
+  ngAfterViewInit(): void {
+    this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+  }
+  //para mostrar opciones de exportacion
+  showPopUpExport = false;
+  //para variar showPopUpExport
+  handlePopUpExport() {
+    this.showPopUpExport = !this.showPopUpExport;
+  }
+
+  optionsExport: { title: string }[] = [
+    {
+      title: 'Exportar gastos',
+    },
+  ];
+
+  //sobre la tabla
+
+  //columnas de tabla
+
+  columns: {
+    key: string;
+    label: string;
+    typeFilter: string;
+    disable?: boolean;
+    options?: string[];
+    ordenable?: boolean;
+  }[] = [
+    {
+      key: 'date',
+      label: 'Fecha',
+      typeFilter: 'input',
+      disable: true,
+    },
+    {
+      key: 'description',
+      label: 'Descripción',
+      typeFilter: 'input',
+    },
+    {
+      key: 'acount',
+      label: 'Cuenta',
+      typeFilter: 'input',
+    },
+
+    {
+      key: 'users',
+      label: 'Usuario',
+      typeFilter: 'input',
+    },
+    {
+      key: 'amount',
+      label: 'Monto',
+      typeFilter: 'input',
+    },
+  ];
+
+  //para la activacion de la tabla
+  isTableEnabled = false;
+
+  //para manejar la activacion de la tabla
+  handleEnabledTable = () => {
+    this.isTableEnabled = !this.isTableEnabled;
+  };
 }
