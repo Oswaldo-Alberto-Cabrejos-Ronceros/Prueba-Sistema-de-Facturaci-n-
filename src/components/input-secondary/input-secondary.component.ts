71ce47710c99400976ff1f1311c5b641f79f7faa
Oswaldo <U22209742@utp.edu.pe>
2025-05-22 17:42:18 -0500

    feat:Adicion de input tamaño para input-secondary

--- src/components/input-secondary/input-secondary.component.ts
+++ src/components/input-secondary/input-secondary.component.ts
@@ -1,9 +1,10 @@
 import { Component, HostBinding, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input-secondary',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './input-secondary.component.html',
   styleUrl: './input-secondary.component.css'
 })
@@ -11,6 +12,7 @@ export class InputSecondaryComponent {
 @Input() type:string="";
 @Input() name:string="";
 @Input() placeholder:string="";
+@Input() size:string="base";
 @HostBinding('class') class='w-full max-w-lg min-w-min'
 value: string = '';
 }
