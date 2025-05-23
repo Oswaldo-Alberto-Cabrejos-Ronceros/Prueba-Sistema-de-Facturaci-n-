c3a3070ca1b630f3f296a6fcdb764b593b4530f1
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 17:32:56 -0500

    feat:Creacion de la carta de emails de destino en reports-accounting-page

--- src/pages/reports-accounting-page/reports-accounting-page.component.ts
+++ src/pages/reports-accounting-page/reports-accounting-page.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { TablePrimaryComponent } from "../../components/table-primary/table-primary.component";
+import { ButtonIconTerciaryComponent } from "../../components/button-icon-terciary/button-icon-terciary.component";
 
 @Component({
   selector: 'app-reports-accounting-page',
-  imports: [],
+  imports: [TablePrimaryComponent, ButtonIconTerciaryComponent],
   templateUrl: './reports-accounting-page.component.html',
   styleUrl: './reports-accounting-page.component.css'
 })
 export class ReportsAccountingPageComponent {
+@HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
+  //columnas de tabla de emails de destino
+  columnsEmailsDestino:string[]=[
+    "Emails destino",
+    "Acciones"
+  ]
 
+  emailsDestinoData:{email:string}[]=[
+    {
+      email:'oswaldocabrejosr@gmail.com'
+    },
+        {
+      email:'oswaldocabrejos15r@gmail.com'
+    }
+  ]
+
+  //columnas de tabla principal
+  columns:string[]=[
+    "Mes",
+    "Resumen de totales",
+    "Resumen detallado(Facturas)",
+    "Compendio (Boletas y notas)",
+    "Compendio (Facturas y notas)",
+    "Libro electrónico de ventas (formato txt)",
+    "Libro electrónico de ventas SIRE (formato zip)"
+  ]
 }
