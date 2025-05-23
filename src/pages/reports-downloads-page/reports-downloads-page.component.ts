0d5ec3c89a62c93584e0a8eed3d3d2e23b96e15a
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 16:40:49 -0500

    feat:Desarrollo de la pagina reports-downloads

--- src/pages/reports-downloads-page/reports-downloads-page.component.ts
+++ src/pages/reports-downloads-page/reports-downloads-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, TemplateRef, ViewChild } from '@angular/core';
+import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
 
 @Component({
   selector: 'app-reports-downloads-page',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './reports-downloads-page.component.css'
 })
 export class ReportsDownloadsPageComponent {
+@HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
+  //referencia al rightTemplatePersonalizado
+  @ViewChild('customRightTemplate', { static: true })
+  customRightTemplate!: TemplateRef<any>;
 
+  //inyectamos servicio template-injector
+  constructor(
+    private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
+  ) {}
+  ngAfterViewInit(): void {
+    this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+  }
 }
