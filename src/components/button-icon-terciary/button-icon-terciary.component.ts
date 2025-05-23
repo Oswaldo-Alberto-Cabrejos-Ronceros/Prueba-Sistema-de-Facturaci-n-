e8b5cc128194214a3d29baba7a59f44102f2c237
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 01:52:12 -0500

    feat:Implementacion de la columna acciones de la tabla de transactions-accounts-page

--- src/components/button-icon-terciary/button-icon-terciary.component.ts
+++ src/components/button-icon-terciary/button-icon-terciary.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-button-icon-terciary',
   imports: [],
   templateUrl: './button-icon-terciary.component.html',
-  styleUrl: './button-icon-terciary.component.css'
+  styleUrl: './button-icon-terciary.component.css',
 })
 export class ButtonIconTerciaryComponent {
-@Input() ariaLabel:string=''
-@Input() iconName:string=''
+  @Input() ariaLabel: string = '';
+  @Input() iconName: string = '';
+  //para exponer el ElementRef
+  @ViewChild('button', { static: true }) buttonRef!: ElementRef;
+  get elementRef(): any {
+    return this.buttonRef;
+  }
 }
--- src/pages/transactions-accounts-page/transactions-accounts-page.component.ts
+++ src/pages/transactions-accounts-page/transactions-accounts-page.component.ts
@@ -1,60 +1,88 @@
-import { Component, HostBinding, TemplateRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
-import { ButtonIconLabelQuaternaryComponent } from "../../components/button-icon-label-quaternary/button-icon-label-quaternary.component";
-import { TableFilterSecondaryComponent } from "../../components/table-filter-secondary/table-filter-secondary.component";
-import { ButtonIconTerciaryComponent } from "../../components/button-icon-terciary/button-icon-terciary.component";
+import { ButtonIconLabelQuaternaryComponent } from '../../components/button-icon-label-quaternary/button-icon-label-quaternary.component';
+import { TableFilterSecondaryComponent } from '../../components/table-filter-secondary/table-filter-secondary.component';
+import { ButtonIconTerciaryComponent } from '../../components/button-icon-terciary/button-icon-terciary.component';
+import { PopUpOptionsComponent } from '../../components/pop-up-options/pop-up-options.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transactions-accounts-page',
-  imports: [ButtonIconLabelQuaternaryComponent, TableFilterSecondaryComponent, ButtonIconTerciaryComponent],
+  imports: [
+    CommonModule,
+    ButtonIconLabelQuaternaryComponent,
+    TableFilterSecondaryComponent,
+    ButtonIconTerciaryComponent,
+    PopUpOptionsComponent,
+  ],
   templateUrl: './transactions-accounts-page.component.html',
-  styleUrl: './transactions-accounts-page.component.css'
+  styleUrl: './transactions-accounts-page.component.css',
 })
 export class TransactionsAccountsPageComponent {
   @HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
-    //referencia al rightTemplatePersonalizado
+  //referencia al rightTemplatePersonalizado
   @ViewChild('customRightTemplate', { static: true })
   customRightTemplate!: TemplateRef<any>;
 
   //inyectamos servicio template-injector
-    constructor(
-      private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
-    ) {}
-    ngAfterViewInit(): void {
-      this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
-    }
+  constructor(
+    private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
+  ) {}
+  ngAfterViewInit(): void {
+    this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+  }
 
-      columns: {
+  columns: {
     key: string;
     label: string;
     typeFilter: string;
     disable?: boolean;
     options?: string[];
     ordenable?: boolean;
-    width?:string
+    width?: string;
   }[] = [
     {
       key: 'name',
       label: 'Nombre',
       typeFilter: 'input',
-      width:'w-[90%]'
+      width: 'w-[90%]',
     },
     {
       key: 'actions',
       label: 'Acciones',
       typeFilter: 'input',
-      disable:true,
-      ordenable:false,
-       width:'w-[10%]'
-    }
+      disable: true,
+      ordenable: false,
+      width: 'w-[10%]',
+    },
   ];
 
-  cuentas=[
+  cuentas = [
     {
-      name:'Luis Alberto',
+      name: 'Luis Alberto',
     },
-        {
-      name:'Carlos Rodrigo',
-    }
-  ]
+    {
+      name: 'Carlos Rodrigo',
+    },
+  ];
+
+  //sobre exportar
+
+  optionsExport: { title: string }[] = [
+    {
+      title: 'Borrar',
+    },
+  ];
+
+  activePopUpIndex: number | null = null;
+
+  handlePopUpExport(index: number) {
+    this.activePopUpIndex = this.activePopUpIndex === index ? null : index;
+  }
 }
