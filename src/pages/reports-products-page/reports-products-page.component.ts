d2d2262ee2fd3166df70bffc7d6a6671342ca61e
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 15:55:16 -0500

    feat:Implementacion de table-filter-secondary en report-products-page

--- src/pages/reports-products-page/reports-products-page.component.ts
+++ src/pages/reports-products-page/reports-products-page.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { TableFilterSecondaryComponent } from "../../components/table-filter-secondary/table-filter-secondary.component";
 
 @Component({
   selector: 'app-reports-products-page',
-  imports: [],
+  imports: [TableFilterSecondaryComponent],
   templateUrl: './reports-products-page.component.html',
-  styleUrl: './reports-products-page.component.css'
+  styleUrl: './reports-products-page.component.css',
 })
 export class ReportsProductsPageComponent {
-
+  columns: {
+    key: string;
+    label: string;
+    typeFilter: string;
+    disable?: boolean;
+    options?: string[];
+    ordenable?: boolean;
+    width?: string;
+  }[] = [
+    {
+      key: 'code',
+      label: 'Código',
+      typeFilter: 'input',
+      width: 'w-[20%]',
+    },
+    {
+      key: 'product',
+      label: 'Producto',
+      typeFilter: 'input',
+      width: 'w-[40%]',
+    },
+    {
+      key: 'last',
+      label: 'Último Precio de venta',
+      typeFilter: 'input',
+      disable: true,
+      ordenable:false,
+      width: 'w-[25%]',
+    },
+    {
+      key: 'total',
+      label: 'Total',
+      typeFilter: 'input',
+      disable: true,
+      width: 'w-[15%]',
+    },
+  ];
 }
