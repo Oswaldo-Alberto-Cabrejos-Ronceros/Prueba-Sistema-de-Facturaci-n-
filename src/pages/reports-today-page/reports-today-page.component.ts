12b70fb372420f12e0c6531dc1889a3efd884586
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 15:42:07 -0500

    feat:Inyeccion de rightTemplate en reports-today

--- src/pages/reports-today-page/reports-today-page.component.ts
+++ src/pages/reports-today-page/reports-today-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, TemplateRef, ViewChild } from '@angular/core';
 import { CardAmountSolesDolarsSetComponent } from '../../components/card-amount-soles-dolars-set/card-amount-soles-dolars-set.component';
 import { CardExportComponent } from "../../components/card-export/card-export.component";
+import { NavBarUserTemplateInjectorService } from '../../services/nav-bar-user-template-injector/nav-bar-user-template-injector.service';
 
 @Component({
   selector: 'app-reports-today-page',
@@ -10,6 +11,17 @@ import { CardExportComponent } from "../../components/card-export/card-export.co
 })
 export class ReportsTodayPageComponent {
   @HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
+    //referencia al rightTemplatePersonalizado
+  @ViewChild('customRightTemplate', { static: true })
+  customRightTemplate!: TemplateRef<any>;
+    //inyectamos servicio template-injector
+    constructor(
+      private navBarUserTemplateInjector: NavBarUserTemplateInjectorService
+    ) {}
+    ngAfterViewInit(): void {
+      this.navBarUserTemplateInjector.setRightTemplate(this.customRightTemplate);
+    }
+
   //datos para la carde de informacion de hoy
   infoToday: {
     iconName: string;
