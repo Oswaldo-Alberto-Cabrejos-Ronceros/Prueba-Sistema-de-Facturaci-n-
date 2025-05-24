923a5b48df3861bd5188aa66dfef742cb487cdbd
Oswaldo <U22209742@utp.edu.pe>
2025-05-23 21:49:41 -0500

    feat:Implementacion de card-send en reports-clients-page

--- src/pages/reports-clients-page/reports-clients-page.component.ts
+++ src/pages/reports-clients-page/reports-clients-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { CardSendComponent } from "../../components/card-send/card-send.component";
 
 @Component({
   selector: 'app-reports-clients-page',
-  imports: [],
+  imports: [CardSendComponent],
   templateUrl: './reports-clients-page.component.html',
   styleUrl: './reports-clients-page.component.css'
 })
 export class ReportsClientsPageComponent {
-
+ @HostBinding('class') class = 'flex-1 flex flex-col min-w-0 gap-4';
 }
