9310b8352897c64575251d9cc72f0c1098ae4e2f
Oswaldo <U22209742@utp.edu.pe>
2025-05-26 20:47:38 -0500

    feat:Desarrollo de business-predetermined-observations

--- src/pages/configurations-business-page/components/business-predetermined-observations-card/business-predetermined-observations-card.component.ts
+++ src/pages/configurations-business-page/components/business-predetermined-observations-card/business-predetermined-observations-card.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TextAreaSecondaryComponent } from "../../../../components/text-area-secondary/text-area-secondary.component";
+import { ButtonSecondaryComponent } from "../../../../components/button-secondary/button-secondary.component";
 
 @Component({
   selector: 'app-business-predetermined-observations-card',
-  imports: [],
+  imports: [TextAreaSecondaryComponent, ButtonSecondaryComponent],
   templateUrl: './business-predetermined-observations-card.component.html',
   styleUrl: './business-predetermined-observations-card.component.css'
 })
