05dd695939ca7b530ea6e6af3b66967d99affa16
Oswaldo <U22209742@utp.edu.pe>
2025-05-26 20:32:55 -0500

    fix:Desarrollo de business-characteristics-card
    Tambien modifique icon component para que reciba una propiedad size

--- src/pages/configurations-business-page/components/business-characteristics-card/business-characteristics-card.component.ts
+++ src/pages/configurations-business-page/components/business-characteristics-card/business-characteristics-card.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CardItemPresentationComponent } from '../../../../components/card-item-presentation/card-item-presentation.component';
+import { SwitchPrimaryComponent } from '../../../../components/switch-primary/switch-primary.component';
+import { CommonModule } from '@angular/common';
+import { InputSecondaryComponent } from "../../../../components/input-secondary/input-secondary.component";
 
 @Component({
   selector: 'app-business-characteristics-card',
-  imports: [],
+  imports: [CommonModule, CardItemPresentationComponent, SwitchPrimaryComponent, InputSecondaryComponent],
   templateUrl: './business-characteristics-card.component.html',
-  styleUrl: './business-characteristics-card.component.css'
+  styleUrl: './business-characteristics-card.component.css',
 })
 export class BusinessCharacteristicsCardComponent {
-
+  //para cartas con switchs
+  cardsWithSwitch: {
+    iconName: string;
+    title: string;
+    content: string;
+    active: boolean;
+  }[] = [
+    {
+      iconName: 'inventory_2',
+      title: 'Stock en modo estricto.',
+      content:
+        'No permite hacer ventas si el stock de los productos está agotado.',
+      active: false,
+    },
+    {
+      iconName: 'credit_card',
+      title: 'Pagos',
+      content: `Agregar forma de pago 'crédito', en Factura/Boleta.`,
+      active: true,
+    },
+    {
+      iconName: 'local_atm',
+      title: 'Agregar BOLETA DE VENTA por defecto en ventas.',
+      content: `En nueva venta por defecto estará seleccionado para crearse una BOLETA DE VENTA.`,
+      active: false,
+    },
+  ];
 }
