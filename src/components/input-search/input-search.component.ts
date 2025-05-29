9da925b746d7fff36dc7f9752e0b9b3789d8f9a2
Oswaldo <U22209742@utp.edu.pe>
2025-05-29 00:51:46 -0500

    feat:Desarrollo de input-search

---
+++ src/components/input-search/input-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { IconComponent } from "../icon/icon.component";
+
+@Component({
+  selector: 'app-input-search',
+  imports: [IconComponent],
+  templateUrl: './input-search.component.html',
+  styleUrl: './input-search.component.css'
+})
+export class InputSearchComponent {
+@HostBinding('class') class='w-full max-w-lg min-w-min flex items-center justify-center'
+//nombre
+@Input() name:string=''
+//placeholder
+@Input() placeholder:string='Buscar'
+}
