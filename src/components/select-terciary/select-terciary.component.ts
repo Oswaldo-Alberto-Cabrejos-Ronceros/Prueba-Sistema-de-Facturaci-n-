46dd01e7f6523d6d429e5d89f9728682db570f2b
Oswaldo <U22209742@utp.edu.pe>
2025-06-01 09:51:07 -0500

    feat:Desarrollo de select-terciary

---
+++ src/components/select-terciary/select-terciary.component.ts
@@ -0,0 +1,17 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-select-terciary',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './select-terciary.component.html',
+  styleUrl: './select-terciary.component.css'
+})
+export class SelectTerciaryComponent {
+  @HostBinding('class') class = 'w-full min-w-min';
+  @Input() name: string = '';
+  @Input() size: string = 'base'; //sm,xs o se deja vacio para que sea base
+  @Input() options: { label: string; value: string | number }[] = [];
+  value: string = '';
+}
