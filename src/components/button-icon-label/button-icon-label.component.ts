f58426d7b2ca2710689e0d4fa81c9882c75fe546
Oswaldo <U22209742@utp.edu.pe>
2025-06-03 00:12:26 -0500

    feat:Adicion de props en button-icon-label para size y border

--- src/components/button-icon-label/button-icon-label.component.ts
+++ src/components/button-icon-label/button-icon-label.component.ts
@@ -1,13 +1,17 @@
 import { Component, HostBinding, Input } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-button-icon-label',
-  imports: [IconComponent],
+  imports: [IconComponent,CommonModule],
   templateUrl: './button-icon-label.component.html',
   styleUrl: './button-icon-label.component.css'
 })
 export class ButtonIconLabelComponent {
 @Input() name:string=''
 @Input() iconName : string=''
+@Input() size:string='base'
+@Input() border:boolean=false
 @HostBinding('class') class='h-12 w-full max-w-lg min-w-min flex items-center justify-center'
 }
